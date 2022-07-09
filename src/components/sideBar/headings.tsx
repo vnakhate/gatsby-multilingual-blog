f6e7eb97e0b4aaed9518f0cd6f8d64b6f55f9edc
Toshiki Kamei <kame0008@gmail.com>
2022-07-09 20:42:36 +0200

    feat: tracking table of contents

--- src/components/sideBar/headings.tsx
+++ src/components/sideBar/headings.tsx
@@ -1,33 +1,38 @@
 /** 1. Imports **/
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import { BlogPostNode } from '../../providers/types/blogPostNode'
+import { useStateHandler } from '../../providers/hooks/useStateHandler'
 
 /** 2. Types **/
-type Props = {
+type Props = ContainerProps & {
+  intersectingElements: Set<string>
+}
+
+type ContainerProps = {
   className?: string
   data: BlogPostNode
 }
 
 /** 3. Base component **/
-const Component = ({ className, data }: Props) => (
+const Component = ({ className, data, intersectingElements }: Props) => (
   <div className={className}>
-    <div className={'side-title'}>Headings</div>
+    <div className={'side-title'}>Table of Contents</div>
     <div>
       {data.headings.map((h) => (
-        <>
-          <span>{`◉`}</span>
+        <React.Fragment key={h.id}>
+          <span className={intersectingElements.has(h.id) ? 'active' : ''}>{`◉`}</span>
           <a href={`#${h.id}`}>
-            <p>{h.value}</p>
+            <p className={intersectingElements.has(h.id) ? 'active' : ''}>{h.value}</p>
           </a>
-        </>
+        </React.Fragment>
       ))}
     </div>
   </div>
 )
 
 /** 4. Styled component **/
-export const Headings = styled(Component)`
+const StyledComponent = styled(Component)`
   position: sticky;
   top: 64px;
 
@@ -37,12 +42,52 @@ export const Headings = styled(Component)`
 
     > span {
       margin: 4px 4px 0 0;
+      color: ${({ theme }) => theme.color.disableGrey};
+
+      transition: color 800ms;
+    }
+    > span.active {
       color: ${({ theme }) => theme.color.highlightMagenta};
     }
 
     p {
       margin-bottom: 24px;
+
+      font-weight: 100;
       font-size: ${({ data }) => (data.fields.language === 'ja' ? 1.4 : 1.6)}rem;
+      color: ${({ theme }) => theme.color.disableGrey};
+
+      transition: color 800ms;
+    }
+    p.active {
+      font-weight: inherit;
+      font-size: ${({ data }) => (data.fields.language === 'ja' ? 1.55 : 1.75)}rem;
+      color: inherit;
     }
   }
 `
+
+export const Headings = (props: ContainerProps) => {
+  const intersectingListHandler = useStateHandler<Set<string>>(new Set())
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        intersectingListHandler.setValue((pre) => {
+          const s: Set<string> = new Set(JSON.parse(JSON.stringify(Array.from(pre))))
+
+          if (entry.isIntersecting) s.add(entry.target.id)
+          if (!entry.isIntersecting) s.delete(entry.target.id)
+
+          return s
+        })
+      })
+    })
+
+    props.data.headings.forEach((h) => {
+      observer.observe(document.getElementById(h.id)!)
+    })
+  }, [])
+
+  return <StyledComponent {...props} intersectingElements={intersectingListHandler.value} />
+}
--- src/providers/hooks/useStateHandler.ts
+++ src/providers/hooks/useStateHandler.ts
@@ -2,16 +2,16 @@ import { useState } from 'react'
 
 export type StateHandler<T> = {
   value: T
-  setValue: (a: T) => void
+  setValue: (value: T | ((previousValue: T) => T)) => void
   reset: () => void
 }
 
-export const useStateHandler = <T>(defaultValue: T, filterFunc?: (a: T) => T): StateHandler<T> => {
+export const useStateHandler = <T>(defaultValue: T): StateHandler<T> => {
   const [state, setState] = useState<T>(defaultValue)
 
   return {
     value: state,
-    setValue: (v) => setState(filterFunc ? filterFunc(v) : v),
+    setValue: (v) => setState(v),
     reset: () => setState(defaultValue),
   }
 }
