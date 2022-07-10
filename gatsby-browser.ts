2d0cb8d47ba495cd23ac203748d40795c9670b68
Toshiki Kamei <kame0008@gmail.com>
2022-07-10 17:08:13 +0200

    add Why-did-you-render

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,5 +1,15 @@
+import React from 'react'
 import { GatsbyBrowser } from 'gatsby'
 import { rootWrapper, pageWrapper } from './gatsby-wrapper'
 
 export const wrapRootElement: GatsbyBrowser['wrapRootElement'] = rootWrapper
 export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = pageWrapper
+
+export const onClientEntry: GatsbyBrowser['onClientEntry'] = () => {
+  if (process.env.NODE_ENV !== 'production') {
+    const whyDidYouRender = require('@welldone-software/why-did-you-render')
+    whyDidYouRender(React, {
+      trackAllPureComponents: true,
+    })
+  }
+}
