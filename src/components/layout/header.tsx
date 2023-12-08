c549eeac43d08e79e76843897fe98e17c08130f1
Venky Nakhate <venky@joviancloud.com>
2023-12-07 18:13:42 -0800

    WIP

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'gatsby-plugin-react-i18next'
-import { LanguageSwitchButton } from '../languageSwitchButton'
+import { LanguageSwitchButton as OriginalLanguageSwitchButton } from '../languageSwitchButton'
 
 /** 2. Types **/
 type Props = {
@@ -54,4 +54,49 @@ export const Header = styled(Component)`
     font-size: 1.4rem;
     color: ${({ theme }) => theme.color.subTextGrey};
   }
+`
+
+/** 5. Modified LanguageSwitchButton component **/
+const LanguageSwitchButtonComponent = ({ className, language, onClick }: Props) =>
+  i18nLanguages.length > 5 ? (
+    <button onClick={onClick}>toggle language</button>
+  ) : (
+    <div className={className} data-language={language} onClick={onClick}>
+      <ul>
+        {i18nLanguages.map((l) => (
+          <li key={l} data-lang={l}>{l.toUpperCase()}</li>
+        ))}
+      </ul>
+    </div>
+  )
+
+/** 6. Styled LanguageSwitchButton component **/
+export const LanguageSwitchButton = styled(LanguageSwitchButtonComponent)`
+  position: relative;
+  width: ${60 * i18nLanguages.length}px;
+  height: 32px;
+  background-color: transparent;
+
+  > ul {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
+    padding: 0 12px;
+    font-size: 1.7rem;
+    color: white;
+
+    > li {
+      transition: color 0.3s ease;
+      margin: 0 5px; // Add some horizontal margin
+
+      ${props => props['data-language'] && css`
+        &[data-lang="${props['data-language']}"] {
+          color: yellow; // Highlight color for selected language
+        }
+      `}
+    }
+  }
+
+  // Rest of the styles...
 `
