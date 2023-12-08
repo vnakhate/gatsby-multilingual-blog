abf799c4f42c14e545c616e9df7e7cc37486d1f3
Venky Nakhate <venky@joviancloud.com>
2023-12-07 18:24:37 -0800

    Revert "WIP"
    This reverts commit c92d9e5da5926ff9a932be8ed453c7b5d8f72be5.

--- src/components/languageSwitchButton.tsx
+++ src/components/languageSwitchButton.tsx
@@ -27,9 +27,40 @@ const Component = ({ className, language, onClick }: Props) =>
 /** 4. Styled component **/
 export const LanguageSwitchButton = styled(Component)`
   position: relative;
-  width: ${60 * i18nLanguages.length}px;
+  width: ${60 * i18nLanguages.length}px; // Adjust this line
   height: 32px;
-  background-color: transparent;
+
+  background-color: hsl(0, 0%, 46%);q
+  border-radius: 12px;
+  box-shadow: inset 2px 2px 6px hsla(0, 0%, 0%, 16%);
+
+  transform: scale(0.9);
+  cursor: pointer;
+
+  > ul {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    height: 100%;
+    padding: 0 12px;
+
+    font-size: 1.7rem;
+    color: white;
+  }
+
+  > div {
+    position: absolute;
+    top: 2px;
+
+    width: 48px;
+    height: 28px;
+
+    background-color: ${({ theme }) => theme.color.backgroundWhite};
+    border-radius: 10px;
+
+    transform: translateX(24px);
+  }
 
   > ul {
     display: flex;
@@ -44,12 +75,11 @@ export const LanguageSwitchButton = styled(Component)`
       transition: color 0.3s ease;
       margin: 0 5px; // Add some horizontal margin
 
-      ${props => props['data-language'] && css`
-        &[data-lang="${props['data-language']}"] {
-          color: yellow; // Highlight color for selected language
-        }
-      `}
+      &[data-lang="${props => props['data-language']}"] {
+        color: yellow; // Highlight color for selected language
+      }
     }
   }
+
   // Rest of the styles...
 `
