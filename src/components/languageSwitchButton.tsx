c92d9e5da5926ff9a932be8ed453c7b5d8f72be5
Venky Nakhate <venky@joviancloud.com>
2023-12-07 18:10:00 -0800

    WIP

--- src/components/languageSwitchButton.tsx
+++ src/components/languageSwitchButton.tsx
@@ -27,40 +27,9 @@ const Component = ({ className, language, onClick }: Props) =>
 /** 4. Styled component **/
 export const LanguageSwitchButton = styled(Component)`
   position: relative;
-  width: ${60 * i18nLanguages.length}px; // Adjust this line
+  width: ${60 * i18nLanguages.length}px;
   height: 32px;
-
-  background-color: hsl(0, 0%, 46%);q
-  border-radius: 12px;
-  box-shadow: inset 2px 2px 6px hsla(0, 0%, 0%, 16%);
-
-  transform: scale(0.9);
-  cursor: pointer;
-
-  > ul {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-
-    height: 100%;
-    padding: 0 12px;
-
-    font-size: 1.7rem;
-    color: white;
-  }
-
-  > div {
-    position: absolute;
-    top: 2px;
-
-    width: 48px;
-    height: 28px;
-
-    background-color: ${({ theme }) => theme.color.backgroundWhite};
-    border-radius: 10px;
-
-    transform: translateX(24px);
-  }
+  background-color: transparent;
 
   > ul {
     display: flex;
@@ -75,11 +44,12 @@ export const LanguageSwitchButton = styled(Component)`
       transition: color 0.3s ease;
       margin: 0 5px; // Add some horizontal margin
 
-      &[data-lang="${props => props['data-language']}"] {
-        color: yellow; // Highlight color for selected language
-      }
+      ${props => props['data-language'] && css`
+        &[data-lang="${props['data-language']}"] {
+          color: yellow; // Highlight color for selected language
+        }
+      `}
     }
   }
-
   // Rest of the styles...
 `
